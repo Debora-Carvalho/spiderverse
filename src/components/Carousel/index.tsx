d0c5bbd26b141627a9c7020b6b0f78838f159a97
Debora-Carvalho <deboravolc2@gmail.com>
2025-07-11 00:20:40 -0300

    🧱 trabalhando na lógica do carrossel

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,5 +1,11 @@
-import { IHeroData } from "@/interfaces/heroes";
+"use client";
+
+import { useEffect, useState } from "react";
 import HeroDetails from "../HeroDetails";
+import styles from "./carousel.module.scss";
+import { IHeroData } from "@/interfaces/heroes";
+import HeroPicture from "../HeroPicture";
+import { AnimatePresence, motion } from "framer-motion";
 
 interface IProps {
   heroes: IHeroData[];
@@ -7,10 +13,52 @@ interface IProps {
 }
 
 export default function Carousel({ heroes, activeId }: IProps) {
+  const [visibleItems, setVisibleItems] = useState<IHeroData[] | null>(null);
+  const [activeIndex, setActiveIndex] = useState<number>(
+    heroes.findIndex((hero) => hero.id === activeId)
+  );
+
+  useEffect(() => {
+    const indexInArrayScope =
+      ((activeIndex % heroes.length) + heroes.length) % heroes.length;
+
+    const visibleItems = [...heroes, ...heroes].slice(
+      indexInArrayScope,
+      indexInArrayScope + 3
+    );
+
+    setVisibleItems(visibleItems);
+  }, [heroes, activeIndex]);
+
+  // altera o herói ativo no carrossel
+  const handleChangeActiveIndex = (newDirection: number) => {
+    setActiveIndex((prevActiveIndex) => prevActiveIndex + newDirection);
+  };
+
+  if (!visibleItems) {
+    return null;
+  }
+
   return (
-    <>
-      <h1>Componente: {activeId}</h1>
-      <HeroDetails data={heroes[0]} />
-    </>
+    <div className={styles.container}>
+      <div className={styles.carousel}>
+        <div
+          className={styles.wrapeer}
+          onClick={() => handleChangeActiveIndex(1)}
+        >
+          <AnimatePresence mode="popLayout">
+            {visibleItems.map((item) => (
+              <motion.div key={item.id} className={styles.hero}>
+                <HeroPicture hero={item} />
+              </motion.div>
+            ))}
+          </AnimatePresence>
+        </div>
+      </div>
+
+      <div className={styles.details}>
+        <HeroDetails data={heroes[0]} />
+      </div>
+    </div>
   );
 }
