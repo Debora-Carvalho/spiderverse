af0a85e90d51d471cfa6a0f1e4402b61a54b1571
Debora-Carvalho <deboravolc2@gmail.com>
2025-07-12 15:12:41 -0300

    ✨ feat:adicionando as animações do carrossel e efeitos sonoros

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,11 +1,17 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import HeroDetails from "../HeroDetails";
 import styles from "./carousel.module.scss";
 import { IHeroData } from "@/interfaces/heroes";
 import HeroPicture from "../HeroPicture";
-import { AnimatePresence, motion } from "framer-motion";
+import { AnimatePresence, motion, scale } from "framer-motion";
+
+enum enPosition {
+  FRONT = 0,
+  MIDDLE = 1,
+  BACK = 2,
+}
 
 interface IProps {
   heroes: IHeroData[];
@@ -15,7 +21,24 @@ interface IProps {
 export default function Carousel({ heroes, activeId }: IProps) {
   const [visibleItems, setVisibleItems] = useState<IHeroData[] | null>(null);
   const [activeIndex, setActiveIndex] = useState<number>(
-    heroes.findIndex((hero) => hero.id === activeId)
+    heroes.findIndex((hero) => hero.id === activeId) - 1
+  );
+  const [startInteractionPosition, setStartInteractionPosition] =
+    useState<number>(0);
+
+  const transitionAudio = useMemo(() => new Audio("/songs/transition.mp3"), []);
+
+  const voicesAudio: Record<string, HTMLAudioElement> = useMemo(
+    () => ({
+      "spider-man-616": new Audio("/songs/spider-man-616.mp3"),
+      "mulher-aranha-65": new Audio("/songs/mulher-aranha-65.mp3"),
+      "spider-man-1610": new Audio("/songs/spider-man-1610.mp3"),
+      "sp-dr-14512": new Audio("/songs/sp-dr-14512.mp3"),
+      "spider-ham-8311": new Audio("/songs/spider-ham-8311.mp3"),
+      "spider-man-90214": new Audio("/songs/spider-man-90214.mp3"),
+      "spider-man-928": new Audio("/songs/spider-man-928.mp3"),
+    }),
+    []
   );
 
   useEffect(() => {
@@ -29,6 +52,71 @@ export default function Carousel({ heroes, activeId }: IProps) {
 
     setVisibleItems(visibleItems);
   }, [heroes, activeIndex]);
+
+  useEffect(() => {
+    const htmlEl = document.querySelector("html");
+
+    if (!htmlEl || !visibleItems) {
+      return;
+    }
+
+    const currentHeroId = visibleItems[enPosition.MIDDLE].id;
+    htmlEl.style.backgroundImage = `url("/spiders/${currentHeroId}-background.png")`;
+    htmlEl.classList.add("hero-page");
+
+    return () => {
+      htmlEl.classList.remove("hero-page");
+    };
+  }, [visibleItems]);
+
+  useEffect(() => {
+    if (!visibleItems) {
+      return;
+    }
+
+    transitionAudio.play();
+
+    const voiceAudio = voicesAudio[visibleItems[enPosition.MIDDLE].id];
+
+    if (!voiceAudio) {
+      return;
+    }
+
+    voiceAudio.volume = 0.3;
+    voiceAudio.play();
+  }, [visibleItems, transitionAudio, voicesAudio]);
+
+  const handleDragStart = (e: React.DragEvent<HTMLDivElement>) => {
+    setStartInteractionPosition(e.clientX);
+  };
+
+  const handleDragEnd = (e: React.DragEvent<HTMLDivElement>) => {
+    if (!startInteractionPosition) {
+      return null;
+    }
+
+    handleChangeDragTouch(e.clientX);
+  };
+
+  const handleTouchStart = (e: React.TouchEvent<HTMLDivElement>) => {
+    setStartInteractionPosition(e.touches[0].clientX);
+  };
+
+  const handleTouchEnd = (e: React.TouchEvent<HTMLDivElement>) => {
+    if (!startInteractionPosition) {
+      return null;
+    }
+
+    handleChangeDragTouch(e.changedTouches[0].clientX);
+  };
+
+  const handleChangeDragTouch = (clientX: number) => {
+    const endInteractionposition = clientX;
+    const diffPosition = endInteractionposition - startInteractionPosition;
+
+    const newPosition = diffPosition > 0 ? -1 : 1;
+    handleChangeActiveIndex(newPosition);
+  };
 
   // altera o herói ativo no carrossel
   const handleChangeActiveIndex = (newDirection: number) => {
@@ -44,11 +132,21 @@ export default function Carousel({ heroes, activeId }: IProps) {
       <div className={styles.carousel}>
         <div
           className={styles.wrapeer}
-          onClick={() => handleChangeActiveIndex(1)}
+          onDragStart={handleDragStart}
+          onDragEnd={handleDragEnd}
+          onTouchStart={handleTouchStart}
+          onTouchEnd={handleTouchEnd}
         >
           <AnimatePresence mode="popLayout">
-            {visibleItems.map((item) => (
-              <motion.div key={item.id} className={styles.hero}>
+            {visibleItems.map((item, position) => (
+              <motion.div
+                key={item.id}
+                className={styles.hero}
+                initial={{ x: -1500, scale: 0.7 }}
+                animate={{ x: 0, ...getItemStyles(position) }}
+                exit={{ x: 0, opacity: 0, scale: 1, left: "-20%" }}
+                transition={{ duration: 0.8 }}
+              >
                 <HeroPicture hero={item} />
               </motion.div>
             ))}
@@ -56,9 +154,42 @@ export default function Carousel({ heroes, activeId }: IProps) {
         </div>
       </div>
 
-      <div className={styles.details}>
-        <HeroDetails data={heroes[0]} />
-      </div>
+      <motion.div
+        className={styles.details}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 1, duration: 2 }}
+      >
+        <HeroDetails data={visibleItems[enPosition.MIDDLE]} />
+      </motion.div>
     </div>
   );
 }
+
+const getItemStyles = (position: enPosition) => {
+  if (position === enPosition.FRONT) {
+    return {
+      zIndex: 3,
+      filter: "blur(10px)",
+      scale: 1.2,
+    };
+  }
+
+  if (position === enPosition.MIDDLE) {
+    return {
+      zIndex: 2,
+      left: 300,
+      scale: 0.8,
+      top: "-10%",
+    };
+  }
+
+  return {
+    zIndex: 1,
+    filter: "blur(10px)",
+    left: 160,
+    top: "-20%",
+    scale: 0.6,
+    opacity: 0.8,
+  };
+};
